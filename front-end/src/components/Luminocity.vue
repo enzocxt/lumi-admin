77922fe44f9758820e996491efd89afd18782fc0
Tao Chen <enzocxt@gmail.com>
2020-06-05 17:33:53 +0200

    add[front-end]: add familiar

--- front-end/src/components/Luminocity.vue
+++ front-end/src/components/Luminocity.vue
@@ -1,16 +1,44 @@
 <template>
   <div>
-    Luminocity
+    <familiar></familiar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Familiar from './home/Familiar'
+
 export default {
   name: 'Luminocity',
-  components: {}
+  components: {
+    Familiar,
+  }
 }
 </script>
 
 <style scoped>
+html {
+  position: relative;
+}
+body, html {
+	/* width:100%; */
+  min-width: 1280px;
+	/*height:100%;*/
+}
+body {
+  height: 100%;
+	margin:0;
+	padding:0;
+  background: linear-gradient(to bottom,#f2d7d2,#ffffff,#f2d7d2,#ffffff,#f2d7d2,#ffffff,#f2d7d2,#ffffff,#f2d7d2);
+  background-repeat:no-repeat
+}
+/* 通用的　*/
+/* 鼠标移动到元素上面显示效果 */
+.pointer {
+  cursor: pointer;
+}
+/* 不显示拥有该CSS类的元素 */
+.cls-hide {
+  display: none;
+}
 </style>
---
+++ front-end/src/components/home/Familiar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="id-familiar">
+    <div id="id-familiar-wrapper">
+      <div id="id-familiar-slideshow" class="pointer">
+      </div>
+      <div id="intro">
+        <a>光明城是同济大学出版社城市、建筑、设计专业出版品牌，由群岛工作室负责策划及出版。致力以更新的出版理念、更敏锐的视角、更积极的态度，回应今天中国城市、建筑、设计领域的问题。</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Familiar',
+  components: {}
+}
+</script>
+
+<style scoped>
+#id-familiar {
+  position: relative;
+	margin-top: 97.5px;
+  margin-bottom: 225px;
+}
+#id-familiar-wrapper {
+  position: relative;
+}
+#id-familiar-slideshow {
+	position: relative;
+  overflow: hidden; /* 添加这行导致margin-right增大至最大值 */
+  width: 600px;
+  height: 427.5px;
+	margin: 0 auto 44px 240px;
+}
+#id-familiar-slideshow img {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#intro {
+  z-index: 9999;
+  position: relative;
+  display: none;
+  width: 780px;
+  margin-left: 240px;
+}
+#intro a{
+  font-family: "SimSun", sans-serif;
+  font-size: 12px;
+  /*letter-spacing:2px;*/
+  line-height: 22.5px
+}
+</style>
